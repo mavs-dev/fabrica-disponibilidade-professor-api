6f9913e341bce149f9b027796dde5e540eaaf2d2
Marcos Vieira <marcosantonio3232@hotmail.com>
2018-09-04 16:25:26 -0300

    #9 Implementação dos parâmetros de execução

--- src/main/java/br/projecao/fabricadesoftware/disponibilidadeprofessoresapi/DisponibilidadeProfessoresApiApplication.java
+++ src/main/java/br/projecao/fabricadesoftware/disponibilidadeprofessoresapi/DisponibilidadeProfessoresApiApplication.java
@@ -3,10 +3,39 @@ package br.projecao.fabricadesoftware.disponibilidadeprofessoresapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.projecao.fabricadesoftware.disponibilidadeprofessoresapi.config.DatabaseConfig;
+
 @SpringBootApplication
 public class DisponibilidadeProfessoresApiApplication {
+	
+	private static final String INPUT_HOST = "-h=";
+	private static final String INPUT_PORT = "-p=";
+	private static final String INPUT_USER = "-u=";
+	private static final String INPUT_PASS = "-pw=";
+	private static final String INPUT_DDL_AUTO = "-ddl=";
+	private static final String INPUT_SHOW_SQL = "-show-sql=";
 
 	public static void main(String[] args) {
+		try {
+			for(String arg: args) {
+				if(arg.contains(INPUT_HOST) && !arg.replace(INPUT_HOST, "").isEmpty()) {
+					DatabaseConfig.URL = DatabaseConfig.URL.replace("localhost", arg.replace(INPUT_HOST, ""));
+				}else if(arg.contains(INPUT_PORT) && !arg.replace(INPUT_PORT, "").isEmpty()) {
+					DatabaseConfig.URL = DatabaseConfig.URL.replace("5432", arg.replace(INPUT_PORT, ""));
+				}else if(arg.contains(INPUT_USER) && !arg.replace(INPUT_USER, "").isEmpty()) {
+					DatabaseConfig.USERNAME = arg.replace(INPUT_USER, "");
+				}else if(arg.contains(INPUT_PASS) && !arg.replace(INPUT_PASS, "").isEmpty()) {
+					DatabaseConfig.PASSWORD = arg.replace(INPUT_PASS, "");
+				}else if(arg.contains(INPUT_DDL_AUTO) && !arg.replace(INPUT_DDL_AUTO, "").isEmpty()) {
+					DatabaseConfig.DDL_AUTO = arg.replace(INPUT_DDL_AUTO, "");
+				}else if(arg.contains(INPUT_SHOW_SQL) && !arg.replace(INPUT_SHOW_SQL, "").isEmpty()) {
+					DatabaseConfig.SHOW_SQL = arg.replace(INPUT_SHOW_SQL, "");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(DisponibilidadeProfessoresApiApplication.class, args);
 	}
+	
 }
---
+++ src/main/java/br/projecao/fabricadesoftware/disponibilidadeprofessoresapi/config/DatabaseConfig.java
@@ -0,0 +1,90 @@
+package br.projecao.fabricadesoftware.disponibilidadeprofessoresapi.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+	
+	public static String URL = "jdbc:postgresql://localhost:5432/db_disponibilidade_professores";
+	public static String USERNAME = "postgres";
+	public static String PASSWORD = "postgres";
+	public static String SHOW_SQL = "false";
+	public static String DDL_AUTO = "validate";
+	
+	@Bean
+	   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	      LocalContainerEntityManagerFactoryBean em 
+	        = new LocalContainerEntityManagerFactoryBean();
+	      em.setDataSource(dataSource());
+	      em.setPackagesToScan(new String[] { "br.projecao.fabricadesoftware.disponibilidadeprofessoresapi.models" });
+	 
+	      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	      ((HibernateJpaVendorAdapter)vendorAdapter).setShowSql(SHOW_SQL.equalsIgnoreCase("true"));
+	      ((HibernateJpaVendorAdapter)vendorAdapter).setGenerateDdl(DDL_AUTO.equalsIgnoreCase("create") || DDL_AUTO.equalsIgnoreCase("create-drop"));
+	      em.setJpaVendorAdapter(vendorAdapter);
+	      em.setJpaProperties(additionalProperties());
+	 
+	      return em;
+	   }
+	 
+	   @Bean
+	   public DataSource dataSource(){
+	      DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	      dataSource.setDriverClassName("org.postgresql.Driver");
+	      dataSource.setUrl(URL);
+	      dataSource.setUsername(USERNAME);
+	      dataSource.setPassword(PASSWORD);
+	      return dataSource;
+	   }
+	 
+	   @Bean
+	   public PlatformTransactionManager transactionManager(
+	     EntityManagerFactory emf){
+	       JpaTransactionManager transactionManager = new JpaTransactionManager();
+	       transactionManager.setEntityManagerFactory(emf);
+	 
+	       return transactionManager;
+	   }
+	 
+	   @Bean
+	   public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+	       return new PersistenceExceptionTranslationPostProcessor();
+	   }
+	 
+	   public Properties additionalProperties() {
+	       Properties properties = new Properties();
+	       properties.setProperty("spring.jpa.database", "POSTGRESQL");
+	       properties.setProperty("spring.jpa.hibernate.ddl-auto", DDL_AUTO);
+	       properties.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+	       properties.setProperty("spring.jpa.show-sql", SHOW_SQL);
+	       properties.setProperty("spring.datasource.validationQuery", "SELECT 1");
+	       properties.put("spring.jpa.properties.hibernate.temp.use_jdbc_metadata_defaults", "false");
+	       properties.put("hibernate.jdbc.lob.non_contextual_creation", "true");
+	       
+	       properties.setProperty("hibernate.database", "POSTGRESQL");
+	       properties.setProperty("hibernate.ddl-auto", DDL_AUTO);
+	       properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+	       properties.setProperty("hibernate.show-sql", SHOW_SQL);
+	       properties.setProperty("hibernate.validationQuery", "SELECT 1");
+	       properties.put("hibernate.temp.use_jdbc_metadata_defaults", "false");
+	       properties.put("hibernate.jdbc.lob.non_contextual_creation", "true");
+	        
+	       return properties;
+	   }
+
+}
